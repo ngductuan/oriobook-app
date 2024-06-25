1f812fa558cc16236c4112666d14cb3476557d54
Nguyễn Đức Tuấn <ndtuan21@clc.fitus.edu.vn>
2024-06-25 12:07:31 +0700

    core: init (ver 2)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -14,17 +10,10 @@ import HelloWorld from '@/components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<style scoped lang="scss">
+@import '@/test.scss';
 </style>
