28181054eb2cec66cca5433630567d2890c64f45
Nguyễn Đức Tuấn <ndtuan21@clc.fitus.edu.vn>
2024-07-03 23:28:17 +0700

    init backend

---
+++ oriobook-backend/src/main/java/com/project/oriobook/OriobookApplication.java
@@ -0,0 +1,13 @@
+package com.project.oriobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriobookApplication.class, args);
+	}
+
+}
---
+++ oriobook-backend/src/test/java/com/project/oriobook/OriobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.oriobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
