561a36c0aafdd5f56b2d4bad5ebdcc4766897b74
Nguyễn Đức Tuấn <ndtuan21@clc.fitus.edu.vn>
2024-07-25 12:34:57 +0700

    fix: update OriobookApplication

--- oriobook-backend/src/main/java/com/project/oriobook/OriobookApplication.java
+++ oriobook-backend/src/main/java/com/project/oriobook/OriobookApplication.java
@@ -1,22 +1,15 @@
 package com.project.oriobook;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication() // exclude = {ReactiveSecurityAutoConfiguration.class}
 public class OriobookApplication {
-    @Value("http://localhost:${server.port}/swagger-ui/index.html")
-    private String swaggerPath;
-
-    private static OriobookApplication instance;
-
-    public OriobookApplication() {
-        instance = this;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(OriobookApplication.class, args);
-        System.out.println("OriobookApplication is running at: " + instance.swaggerPath);
     }
 }
