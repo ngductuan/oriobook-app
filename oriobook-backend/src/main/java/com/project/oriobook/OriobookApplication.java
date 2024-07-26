fb6c197d4f19da3a4f904c14dd0ffbdc3db7c328
Nguyễn Đức Tuấn <ndtuan21@clc.fitus.edu.vn>
2024-07-26 17:24:14 +0700

    fix: fix recursive of Category.java. Adjust: jpa:            open-in-view: false

--- oriobook-backend/src/main/java/com/project/oriobook/OriobookApplication.java
+++ oriobook-backend/src/main/java/com/project/oriobook/OriobookApplication.java
@@ -1,11 +1,22 @@
 package com.project.oriobook;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication() // exclude = {ReactiveSecurityAutoConfiguration.class}
 public class OriobookApplication {
+    @Value("http://localhost:${server.port}/swagger-ui/index.html")
+    private String swaggerPath;
+
+    private static OriobookApplication instance;
+
+    public OriobookApplication() {
+        instance = this;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(OriobookApplication.class, args);
+        System.out.println("OriobookApplication is running at: " + instance.swaggerPath);
     }
 }
