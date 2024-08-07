72d4c426a63e1b9829d8877e9cdda1b70b0348ad
Nguyen Duc Tuan <ndtuan21@clc.fitus.edu.vn>
2024-08-08 00:19:02 +0700

    fix: fix api.prefix

---
+++ oriobook-backend/src/main/java/com/project/oriobook/common/configs/cors/ProdCorsConfig.java
@@ -0,0 +1,22 @@
+package com.project.oriobook.common.configs.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@Profile("prod")
+public class ProdCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("ProdCorsConfig");
+        registry.addMapping("/**")
+                .allowedOrigins("https://web.orio-studio.online")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
