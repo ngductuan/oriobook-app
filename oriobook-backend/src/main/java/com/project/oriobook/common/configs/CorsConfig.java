2e9274e360198204689bca0772a78f736baec08d
Nguyen Duc Tuan <ndtuan21@clc.fitus.edu.vn>
2024-08-07 22:21:55 +0700

    fix: update cors config (open at all)

--- oriobook-backend/src/main/java/com/project/oriobook/common/configs/CorsConfig.java
+++ oriobook-backend/src/main/java/com/project/oriobook/common/configs/CorsConfig.java
@@ -5,22 +5,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
-public class CorsConfig {
-    // @Value("${spring.client-url.web}")
-    // private String webClientUrl;
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        // config.addAllowedOrigin(webClientUrl); // Adjust this to your frontend URL
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Adjust this as needed
+                .allowedMethods("*") // Adjust this as needed
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
+
+// @Configuration
+// public class CorsConfig {
+//     // @Value("${spring.client-url.web}")
+//     // private String webClientUrl;
+//
+//     @Bean
+//     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         CorsConfiguration config = new CorsConfiguration();
+//         config.setAllowCredentials(true);
+//         // config.addAllowedOrigin(webClientUrl); // Adjust this to your frontend URL
+//         config.addAllowedOrigin("*");
+//         config.addAllowedHeader("*");
+//         config.addAllowedMethod("*");
+//         source.registerCorsConfiguration("/**", config);
+//         return source;
+//     }
+// }
